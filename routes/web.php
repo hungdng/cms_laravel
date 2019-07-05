59180fa8a848f5709208fba05c89e55155339804
HungTran <hungdng.92@gmail.com>
2019-07-05 14:21:17 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/','PostController@all');
 Route::any('posts/{post}', 'PostController@single');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin/{any}', 'AdminController@index')->where('any', '.*');
+
+Route::get('/{post}/comments', 'CommentController@index');
+Route::post('/{post}/comments', 'CommentController@store');
